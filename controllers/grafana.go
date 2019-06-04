97d909d8fdbf708102ae3d5f37aa5ca14ba12540
zhangjikun <zhangjikun@haima.me>
2019-06-04 11:55:07 +0800

    新增默认通知号码设置

---
+++ controllers/grafana.go
@@ -0,0 +1,141 @@
+package controllers
+
+import (
+	"bufio"
+	"encoding/json"
+	"github.com/astaxie/beego"
+	"io"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+type GrafanaController struct {
+	beego.Controller
+}
+
+// {"evalMatches":[],"message":"5分钟内申请云服务流量低于100","ruleId":6,"ruleName":"云服务任务成功数量过低","ruleUrl":"http://grafana.haimacloud.com/d/pH9lfnrmk/ias-p3-ji-gao-jing-xiang?fullscreen=true\u0026edit=true\u0026tab=alert\u0026panelId=28\u0026orgId=1","state":"ok","title":"[OK] 云服务任务成功数量过低"}
+//{"evalMatches":[{"value":0,"metric":"Count","tags":{}}],"message":"5分钟内申请云服务流量低于100","ruleId":6,"ruleName":"云服务任务成功数量过低","ruleUrl":"http://grafana.haimacloud.com/d/pH9lfnrmk/ias-p3-ji-gao-jing-xiang?fullscreen=true\u0026edit=true\u0026tab=alert\u0026panelId=28\u0026orgId=1","state":"alerting","title":"[Alerting] 云服务任务成功数量过低"}
+
+type Grafana struct {
+	Message string `json:"message"`
+	RuleName string `json:"ruleName"`
+	RuleUrl string `json:"ruleUrl"`
+	State string `json:"state"`
+}
+
+func (c *GrafanaController) GrafanaAlert() {
+	alert:=Grafana{}
+	log.SetPrefix("[DEBUG 1]")
+	log.Println(string(c.Ctx.Input.RequestBody))
+	json.Unmarshal(c.Ctx.Input.RequestBody, &alert)
+	c.Data["json"]=SendMessageGrafana(alert)
+	log.SetPrefix("[DEBUG 3]")
+	log.Println(c.Data["json"])
+	c.ServeJSON()
+}
+
+func SendMessageGrafana(message Grafana)(string)  {
+	Title:=beego.AppConfig.String("title")
+	Logourl:=beego.AppConfig.String("logourl")
+	Defaultphone:=beego.AppConfig.String("defaultphone")
+	text:=""
+	//MobileMessage:=""
+	PhoneCallMessage:=""
+	titleend:=""
+	returnMessage:=""
+
+	//拨号的手机号码
+	phone:=""
+	//告警级别定义 0 信息,1 警告,2 一般严重,3 严重,4 灾难
+	AlertLevel:=[]string{"信息","警告","一般严重","严重","灾难"}
+
+	//拆分用户号码和告警消息
+	fullMessage:=strings.Split(message.Message,"&&ddurl")
+
+	if message.State=="ok" {
+		titleend="故障恢复信息"
+		text="## ["+Title+"Grafana"+titleend+"]("+message.RuleUrl+")\n\n"+"#### "+message.RuleName+"\n\n"+"###### 告警级别："+AlertLevel[4]+"\n\n"+"###### 开始时间："+time.Now().Format("2006-01-02 15:04:05")+"\n\n"+"##### "+fullMessage[0]+" 已经恢复正常\n\n"+"!["+Title+"]("+Logourl+")"
+		//MobileMessage="\n["+Title+"Grafana"+titleend+"]\n"+message.RuleName+"\n"+"告警级别："+AlertLevel[4]+"\n"+"开始时间："+time.Now().Format("2006-01-02 15:04:05")+"\n"+message.Message+" 已经恢复正常"
+		PhoneCallMessage=fullMessage[0]+" 已经恢复正常"
+	}else {
+		titleend="故障告警信息"
+		text="## ["+Title+"Grafana"+titleend+"]("+message.RuleUrl+")\n\n"+"#### "+message.RuleName+"\n\n"+"###### 告警级别："+AlertLevel[4]+"\n\n"+"###### 开始时间："+time.Now().Format("2006-01-02 15:04:05")+"\n\n"+"##### "+fullMessage[0]+"\n\n"+"!["+Title+"]("+Logourl+")"
+		//MobileMessage="\n["+Title+"Grafana"+titleend+"]\n"+message.RuleName+"\n"+"告警级别："+AlertLevel[4]+"\n"+"开始时间："+time.Now().Format("2006-01-02 15:04:05")+"\n"+message.Message
+		PhoneCallMessage=fullMessage[0]
+	}
+
+	//根据不同nLevel 优先级告警
+	//if (nLevel==Messagelevel) {
+	//	return PostTXmessage(MobileMessage,AlerMessage[0].Annotations.Mobile)
+	//} else if (nLevel==PhoneCalllevel) {
+	//	return PostTXphonecall(PhoneCallMessage,AlerMessage[0].Annotations.Mobile)
+	//}
+	//return PostToDingDing(Title+titleend,text)
+
+	//拆分ddurl
+	url:=beego.AppConfig.String("ddurl")
+	//判断发送到默认钉钉 还是多个钉钉
+	if len(fullMessage)<2 {
+		returnMessage=returnMessage+"PostToDingDing:"+PostToDingDing(Title+titleend,text,url)+"\n"
+	} else {
+		DD:=strings.Split(fullMessage[1], ",")
+		for _,d:=range DD {
+			returnMessage=returnMessage+"PostToDingDing:"+PostToDingDing(Title+titleend,text,d)+"\n"
+		}
+	}
+	//取到手机号
+	if Defaultphone=="" {
+		phone=GetUserPhone()
+	} else {
+		phone=Defaultphone
+	}
+
+	returnMessage=returnMessage+"PostTXphonecall:"+PostTXphonecall(PhoneCallMessage,phone)+"\n"
+	return returnMessage
+}
+
+//获取用户号码
+func GetUserPhone() string  {
+	//判断是否存在user.csv文件
+	Num:=""
+	Today:=time.Now()
+	//Today:=time.Now().Format("2006年1月2日")
+	//判断当前时间是否大于10点,大于10点取当天值班号码,小于10点取前一天值班号码
+	DayString:=""
+	if time.Now().Hour()>=10 {
+		//取当天值班号码
+		DayString=Today.Format("2006年1月2日")
+	} else {
+		//取前一天值班号码
+		DayString=Today.AddDate(0,0,-1).Format("2006年1月2日")
+	}
+
+	_, err := os.Stat("user.csv")
+	if err == nil {
+		f, err := os.Open("user.csv")
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+
+		rd := bufio.NewReader(f)
+		for {
+			line, err := rd.ReadString('\n') //以'\n'为结束符读入一行
+			if err != nil || io.EOF == err {
+				Num="15110176396"
+			} else {
+				if strings.Contains(line,DayString ) {
+					x:=strings.Split(line, ",")
+					Num=x[1]
+					break
+				}
+			}
+		}
+
+	}else {
+		Num="15110176396"
+	}
+	return Num
+}
---
+++ controllers/prometheus.go
@@ -0,0 +1,124 @@
+package controllers
+
+import (
+	"encoding/json"
+	"github.com/astaxie/beego"
+	"log"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type PrometheusController struct {
+	beego.Controller
+}
+
+type Labels struct{
+	Alertname string `json:"alertname"`
+	Instance string `json:"instance"`
+}
+type Annotations struct{
+	Description string `json:"description"`
+	Summary string `json:"summary"`
+	Level string `json:"level"`  //2019年2月15日 19:03:07 增加告警级别
+	Mobile string `json:"mobile"` //2019年2月25日 19:09:23 增加手机号支持
+	Ddurl string `json:"ddurl"` //2019年3月12日 20:33:38 增加多个钉钉告警支持
+}
+type Alerts struct {
+	Labels Labels `json:"labels"`
+	Annotations Annotations `json:"annotations"`
+	StartsAt string `json:"startsAt"`
+	EndsAt string `json:"endsAt"`
+}
+type Prometheus struct {
+	Status string
+	Alerts []Alerts
+}
+
+
+// 按照 Alert.Level 从大到小排序
+type AlerMessages [] Alerts
+
+func (a AlerMessages) Len() int {         // 重写 Len() 方法
+	return len(a)
+}
+func (a AlerMessages) Swap(i, j int){     // 重写 Swap() 方法
+	a[i], a[j] = a[j], a[i]
+}
+func (a AlerMessages) Less(i, j int) bool {    // 重写 Less() 方法， 从大到小排序
+	return a[j].Annotations.Level < a[i].Annotations.Level
+}
+
+//for prometheus alert
+//关于告警级别level共有5个级别,0-4,0 信息,1 警告,2 一般严重,3 严重,4 灾难
+func (c *PrometheusController) PrometheusAlert() {
+	//{"receiver":"web\\.hook","status":"firing","alerts":[{"status":"firing","labels":{"alertname":"Node_alert","instance":"192.168.10.5:9100","job":"node1","monitor":"node1","node":"alert"},"annotations":{"description":"If one more node goes down the node will be unavailable","summary":"192.168.10.5:9100 node goes down!(current value: 0.2s)"},"startsAt":"2018-08-01T02:01:44.71271343-04:00","endsAt":"0001-01-01T00:00:00Z","generatorURL":"http://localhost.localdomain:9090/graph?g0.expr=node_load1+%3E+0.1\u0026g0.tab=1"}],"groupLabels":{"alertname":"Node_alert"},"commonLabels":{"alertname":"Node_alert","instance":"192.168.10.5:9100","job":"node1","monitor":"node1","node":"alert"},"commonAnnotations":{"description":"If one more node goes down the node will be unavailable","summary":"192.168.10.5:9100 node goes down!(current value: 0.2s)"},"externalURL":"http://localhost.localdomain:9093","version":"4","groupKey":"{}:{alertname=\"Node_alert\"}"}
+	alert:=Prometheus{}
+	log.SetPrefix("[DEBUG 1]")
+	log.Println(string(c.Ctx.Input.RequestBody))
+	json.Unmarshal(c.Ctx.Input.RequestBody, &alert)
+	c.Data["json"]=SendMessageP(alert)
+	log.SetPrefix("[DEBUG 3]")
+	log.Println(c.Data["json"])
+	c.ServeJSON()
+}
+func GetCSTtime(date string)(string)  {
+	T1:=date[0:10]
+	T2:=date[11:19]
+	T3:=T1+" "+T2
+	tm2, _ := time.Parse("2006-01-02 15:04:05", T3)
+	h, _ := time.ParseDuration("-1h")
+	tm3:=tm2.Add(-8*h)
+	tm:=tm3.Format("2006-01-02 15:04:05")
+	return tm
+}
+func SendMessageP(message Prometheus)(string)  {
+	Title:=beego.AppConfig.String("title")
+	Alerturl:=beego.AppConfig.String("alerturl")
+	Logourl:=beego.AppConfig.String("logourl")
+	Messagelevel,_:=beego.AppConfig.Int("messagelevel")
+	PhoneCalllevel,_:=beego.AppConfig.Int("phonecalllevel")
+	text:=""
+	MobileMessage:=""
+	PhoneCallMessage:=""
+	titleend:=""
+	returnMessage:=""
+	//对分组消息进行排序
+	AlerMessage:=message.Alerts
+	sort.Sort(AlerMessages(AlerMessage))
+	//nLevel 为第一条告警信息的告警级别
+	nLevel,_:=strconv.Atoi(AlerMessage[0].Annotations.Level)
+	//告警级别定义 0 信息,1 警告,2 一般严重,3 严重,4 灾难
+	AlertLevel:=[]string{"信息","警告","一般严重","严重","灾难"}
+
+	if message.Status=="resolved" {
+		titleend="故障恢复信息"
+		text="## ["+Title+"Prometheus"+titleend+"]("+Alerturl+")\n\n"+"#### "+AlerMessage[0].Labels.Alertname+"\n\n"+"###### 告警级别："+AlertLevel[nLevel]+"\n\n"+"###### 开始时间："+GetCSTtime(AlerMessage[0].StartsAt)+"\n\n"+"###### 结束时间："+GetCSTtime(AlerMessage[0].EndsAt)+"\n\n"+"###### 故障主机IP："+AlerMessage[0].Labels.Instance+"\n\n"+"##### "+AlerMessage[0].Annotations.Summary+"\n\n"+"!["+Title+"]("+Logourl+")"
+		MobileMessage="\n["+Title+"Prometheus"+titleend+"]\n"+AlerMessage[0].Labels.Alertname+"\n"+"告警级别："+AlertLevel[nLevel]+"\n"+"开始时间："+GetCSTtime(AlerMessage[0].StartsAt)+"\n"+"结束时间："+GetCSTtime(AlerMessage[0].EndsAt)+"\n"+"故障主机IP："+AlerMessage[0].Labels.Instance+"\n"+AlerMessage[0].Annotations.Summary
+		PhoneCallMessage="故障主机IP："+AlerMessage[0].Labels.Instance+","+AlerMessage[0].Annotations.Summary
+	}else {
+		titleend="故障告警信息"
+		text="## ["+Title+"Prometheus"+titleend+"]("+Alerturl+")\n\n"+"#### "+AlerMessage[0].Labels.Alertname+"\n\n"+"###### 告警级别："+AlertLevel[nLevel]+"\n\n"+"###### 开始时间："+GetCSTtime(AlerMessage[0].StartsAt)+"\n\n"+"###### 结束时间："+GetCSTtime(AlerMessage[0].EndsAt)+"\n\n"+"###### 故障主机IP："+AlerMessage[0].Labels.Instance+"\n\n"+"##### "+AlerMessage[0].Annotations.Description+"\n\n"+"!["+Title+"]("+Logourl+")"
+		MobileMessage="\n["+Title+"Prometheus"+titleend+"]\n"+AlerMessage[0].Labels.Alertname+"\n"+"告警级别："+AlertLevel[nLevel]+"\n"+"开始时间："+GetCSTtime(AlerMessage[0].StartsAt)+"\n"+"结束时间："+GetCSTtime(AlerMessage[0].EndsAt)+"\n"+"故障主机IP："+AlerMessage[0].Labels.Instance+"\n"+AlerMessage[0].Annotations.Description
+		PhoneCallMessage="故障主机IP："+AlerMessage[0].Labels.Instance+","+AlerMessage[0].Annotations.Description
+	}
+	if AlerMessage[0].Annotations.Ddurl==""{
+		url:=beego.AppConfig.String("ddurl")
+		returnMessage=returnMessage+"PostToDingDing:"+PostToDingDing(Title+titleend,text,url)+"\n"
+	}else {
+		Ddurl:=strings.Split(AlerMessage[0].Annotations.Ddurl,",")
+		for _,url:=range Ddurl{
+			returnMessage=returnMessage+"PostToDingDing:"+PostToDingDing(Title+titleend,text,url)+"\n"
+		}
+	}
+	if (nLevel==Messagelevel) {
+		returnMessage=returnMessage+"PostTXmessage:"+PostTXmessage(MobileMessage,AlerMessage[0].Annotations.Mobile)+"\n"
+	} else if (nLevel==PhoneCalllevel) {
+		returnMessage=returnMessage+"PostTXphonecall:"+PostTXphonecall(PhoneCallMessage,AlerMessage[0].Annotations.Mobile)+"\n"
+	}
+	return returnMessage
+}
+
+
+
