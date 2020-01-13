c504f1bc98890ca61bb7d06b2e38c7c86cd2d097
jikun.zhang <jikun.zhang@megatronix.co>
2020-01-13 16:25:04 +0800

    更新graylog2和graylog3的显示字段

--- controllers/graylog3.go
+++ controllers/graylog3.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/logs"
+	"strconv"
 )
 
 type Graylog3Controller struct {
@@ -11,17 +12,25 @@ type Graylog3Controller struct {
 }
 //graylog3告警部分
 type Graylog3 struct {
-	Title string `json:"event_definition_title"`
 	Description string `json:"event_definition_description"`
 	Event AlertEvent `json:"event"`
-	//backlog alertBacklog `json:"backlog"`
+	Backlogs []Backlog `json:"backlog"`
 }
 type AlertEvent struct {
-	Timestamp string `json:"timestamp"` //开始时间
-	Timestamp_processing string `json:"timestamp_processing"` //持续时间
-	Message string `json:"message"`
 	Source string `json:"source`
 }
+type Backlog struct {
+	Timestamp string `json:"timestamp"` //开始时间
+	Index string `json:"index"` //索引
+	Message string `json:"message"`
+	Fields G3Field `json:"fields`
+}
+type G3Field struct {
+	Gl2RemoteIp string `json:"gl2_remote_ip"`
+	Gl2RemotePort int `json:"gl_2_remote_port"`
+}
+
+
 //for graylog3 alert
 func (c *Graylog3Controller) GraylogDingding() {
 	alert:=Graylog3{}
@@ -89,40 +98,42 @@ func (c *Graylog3Controller) GraylogALYdh() {
 func SendMessageG3(message Graylog3,typeid int,logsign string)(string)  {
 	Title:=beego.AppConfig.String("title")
 	Logourl:=beego.AppConfig.String("logourl")
-	DDtext:="## ["+Title+"Graylog3告警信息]("+message.Event.Source+")\n\n"+"#### "+message.Description+"\n\n"+"###### 告警名称："+message.Title+"\n\n"+"###### 开始时间："+message.Event.Timestamp+" \n\n"+"###### 持续时间："+message.Event.Timestamp_processing+"\n\n"+"!["+Title+"]("+Logourl+")"
-	WXtext:="["+Title+"Graylog3告警信息]("+message.Event.Source+")\n>**"+message.Description+"**\n>`告警名称:`"+message.Title+"\n`开始时间:`"+message.Event.Timestamp+" \n`持续时间:`"+message.Event.Timestamp_processing+"\n"
-	PhoneCallMessage=message.Description
-	//触发钉钉
-	if typeid==2 {
-		ddurl:=beego.AppConfig.String("ddurl")
-		PostToDingDing(Title+"告警信息", DDtext, ddurl,logsign)
-	}
-	//触发微信
-	if typeid==3 {
-		wxurl:=beego.AppConfig.String("wxurl")
-		PostToWeiXin(WXtext, wxurl,logsign)
-	}
-	//取到手机号
-	phone:=GetUserPhone(1)
-	//触发电话告警
-	if typeid==4 {
-		PostTXphonecall(PhoneCallMessage,phone,logsign)
-	}
-	//触发腾讯云短信告警
-	if typeid==5 {
-		PostTXmessage(PhoneCallMessage,phone,logsign)
-	}
-	//触发华为云短信告警
-	if typeid==6 {
-		PostHWmessage(PhoneCallMessage,phone,logsign)
-	}
-	//触发阿里云短信告警
-	if typeid==7 {
-		PostALYmessage(PhoneCallMessage,phone,logsign)
-	}
-	//触发阿里云电话告警
-	if typeid==8 {
-		PostALYphonecall(PhoneCallMessage,phone,logsign)
+	for _, m := range message.Backlogs {
+		DDtext:="## ["+Title+"Graylog3告警信息]("+message.Event.Source+")\n\n"+"#### "+m.Message+"\n\n"+"###### 告警名称："+message.Description+"\n\n"+"###### 告警索引："+m.Index+"\n\n"+"###### 开始时间："+m.Timestamp+" \n\n"+"###### 告警主机："+m.Fields.Gl2RemoteIp+":"+strconv.Itoa(m.Fields.Gl2RemotePort)+"\n\n"+"!["+Title+"]("+Logourl+")"
+		WXtext:="["+Title+"Graylog3告警信息]("+message.Event.Source+")\n>**"+m.Message+"**\n>`告警名称:`"+message.Description+"\n`告警索引:`"+m.Index+"\n`开始时间:`"+m.Timestamp+" \n`告警主机:`"+m.Fields.Gl2RemoteIp+":"+strconv.Itoa(m.Fields.Gl2RemotePort)+"\n"
+		PhoneCallMessage="告警主机:"+m.Fields.Gl2RemoteIp+":"+strconv.Itoa(m.Fields.Gl2RemotePort)+"告警消息:"+m.Message
+		//触发钉钉
+		if typeid==2 {
+			ddurl:=beego.AppConfig.String("ddurl")
+			PostToDingDing(Title+"告警信息", DDtext, ddurl,logsign)
+		}
+		//触发微信
+		if typeid==3 {
+			wxurl:=beego.AppConfig.String("wxurl")
+			PostToWeiXin(WXtext, wxurl,logsign)
+		}
+		//取到手机号
+		phone:=GetUserPhone(1)
+		//触发电话告警
+		if typeid==4 {
+			PostTXphonecall(PhoneCallMessage,phone,logsign)
+		}
+		//触发腾讯云短信告警
+		if typeid==5 {
+			PostTXmessage(PhoneCallMessage,phone,logsign)
+		}
+		//触发华为云短信告警
+		if typeid==6 {
+			PostHWmessage(PhoneCallMessage,phone,logsign)
+		}
+		//触发阿里云短信告警
+		if typeid==7 {
+			PostALYmessage(PhoneCallMessage,phone,logsign)
+		}
+		//触发阿里云电话告警
+		if typeid==8 {
+			PostALYphonecall(PhoneCallMessage,phone,logsign)
+		}
 	}
 	return "告警消息发送完成."
 }
