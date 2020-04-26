d4eb39a96cbc56dda1e2901d59ed0372753db93b
jikun.zhang <jikun.zhang@megatronix.co>
2020-04-26 11:20:20 +0800

    更新zabbix对飞书和容联云电话的支持

--- controllers/zabbix.go
+++ controllers/zabbix.go
@@ -11,11 +11,12 @@ type ZabbixController struct {
 }
 
 type ZabbixMessage struct{
-	ZabbixTarget string `json:"zabbixtarget"`
-	ZabbixMessage string `json:"zabbixmessage"`
-	ZabbixType string `json:"zabbixtype"`
+	ZabbixTarget string `json:"zabbixtarget"` //告警目标
+	ZabbixMessage string `json:"zabbixmessage"`  //告警消息
+	ZabbixType string `json:"zabbixtype"`     //告警类型
 }
 
+//zabbix告警消息入口
 func (c *ZabbixController) ZabbixAlert() {
 	alert:=ZabbixMessage{}
 	logsign:="["+LogsSign()+"]"
@@ -25,22 +26,36 @@ func (c *ZabbixController) ZabbixAlert() {
 	logs.Info(logsign,c.Data["json"])
 	c.ServeJSON()
 }
+
 func SendMessageZabbix(message ZabbixMessage,logsign string) (string){
 	ret:=""
 	switch message.ZabbixType {
+	//微信渠道
 	case "wx":
 		ret=PostToWeiXin(message.ZabbixMessage,message.ZabbixTarget,logsign)
+	//钉钉渠道
 	case "dd":
 		ret=PostToDingDing("Zabbix告警消息",message.ZabbixMessage,message.ZabbixTarget,logsign)
+	//飞书渠道
 	case "fs":
 		ret=PostToFeiShu("Zabbix告警消息",message.ZabbixMessage,message.ZabbixTarget,logsign)
+	//短信渠道
 	case "dx":
+		//腾讯云短信
 		ret=PostTXmessage(message.ZabbixMessage,message.ZabbixTarget,logsign)
+		//华为云短信
 		ret=ret+PostHWmessage(message.ZabbixMessage,message.ZabbixTarget,logsign)
+		//阿里云短信
 		ret=ret+PostALYmessage(message.ZabbixMessage,message.ZabbixTarget,logsign)
+	//电话渠道
 	case "dh":
+		//腾讯云电话
 		ret=PostTXphonecall(message.ZabbixMessage,message.ZabbixTarget,logsign)
+		//阿里云电话
 		ret=ret+PostALYphonecall(message.ZabbixMessage,message.ZabbixTarget,logsign)
+		//容联云电话
+		ret=ret+PostRLYphonecall(message.ZabbixMessage,message.ZabbixTarget,logsign)
+	//异常参数
 	default:
 		ret="参数错误"
 	}
--- zabbix/zabbixclient.go
+++ zabbix/zabbixclient.go
@@ -15,7 +15,7 @@ func init()  {
 	//zabbix传入参数发送目标，告警消息，目标类型
 	flag.StringVar(&ZabbixTarget, "t", "https://oapi.dingtalk.com/robot/send?access_token=xxxxx", "指定告警消息的接收目标的`手机号/钉钉url/微信url`")
 	flag.StringVar(&ZabbixMessage, "m", "zabbix告警测试", "需要发送的`告警消息内容`")
-	flag.StringVar(&ZabbixType, "type", "dd", "告警消息的目标类型,支持`dx(短信)、dh(电话)、dd(钉钉)、wx(微信)`")
+	flag.StringVar(&ZabbixType, "type", "dd", "告警消息的目标类型,支持`dx(短信)、dh(电话)、dd(钉钉)、wx(微信)、fs(飞书)`")
 	flag.StringVar(&PrometheusAlertUrl, "d", "http://127.0.0.1:8080/zabbix", "`PrometheusAlert的地址`")
 	flag.BoolVar(&help,"h", false, "显示帮助")
 	flag.Usage = usage
@@ -23,7 +23,7 @@ func init()  {
 func usage() {
 	fmt.Fprintf(os.Stderr, `Version 1.0 If you need help contact 244217140@qq.com or visit https://github.com/feiyu563/PrometheusAlert
 Usage: zabbixclient [-h] [-t SendTarget] [-m SendMessage] [-type SendType] [-d PrometheusAlertUrl]
-Example(发送告警到钉钉)：zabbixclent -t https://oapi.dingtalk.com/robot/send?access_token=xxxxx -m zabbix告警测试 -type dh -d http://127.0.0.1:8080/zabbix
+Example(发送告警到钉钉)：zabbixclent -t https://oapi.dingtalk.com/robot/send?access_token=xxxxx -m zabbix告警测试 -type dd -d http://127.0.0.1:8080/zabbix
 
 Options:
 `)
