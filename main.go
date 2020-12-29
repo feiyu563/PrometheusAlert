33e99eae08387ec3ec94bfc22e86dd942ac21dba
jikun.zhang <jikun.zhang@megatronix.co>
2020-12-29 17:36:02 +0800

    更新默认数据存储配置

--- main.go
+++ main.go
@@ -8,15 +8,17 @@ import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/logs"
 	"github.com/astaxie/beego/orm"
-	_ "github.com/mattn/go-sqlite3"
 	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"os"
 	"path"
 )
 
 func init() {
-	if beego.AppConfig.String("db_driver")=="sqlite3" {
+	db_driver:=beego.AppConfig.String("db_driver")
+	switch db_driver {
+	case "sqlite3":
 		// 检查数据库文件
 		Db_name := "./db/PrometheusAlertDB.db"
 		if !com.IsExist(Db_name) {
@@ -27,9 +29,20 @@ func init() {
 		orm.RegisterDriver("db_driver", orm.DRSqlite)
 		// 注册默认数据库
 		orm.RegisterDataBase("default", "sqlite3", Db_name, 10)
-	}else {
+	case "mysql":
 		orm.RegisterDriver("mysql", orm.DRMySQL)
 		orm.RegisterDataBase("default", "mysql", beego.AppConfig.String("db_user")+":"+beego.AppConfig.String("db_password")+"@tcp("+beego.AppConfig.String("db_host")+")/"+beego.AppConfig.String("db_name")+"?charset=utf8mb4")
+	default:
+		// 检查数据库文件
+		Db_name := "./db/PrometheusAlertDB.db"
+		if !com.IsExist(Db_name) {
+			os.MkdirAll(path.Dir(Db_name), os.ModePerm)
+			os.Create(Db_name)
+		}
+		// 注册驱动（“sqlite3” 属于默认注册，此处代码可省略）
+		orm.RegisterDriver("db_driver", orm.DRSqlite)
+		// 注册默认数据库
+		orm.RegisterDataBase("default", "sqlite3", Db_name, 10)
 	}
 	// 注册模型
 	orm.RegisterModel(new(models.PrometheusAlertDB))
