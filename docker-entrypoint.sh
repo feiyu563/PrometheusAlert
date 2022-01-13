04b8a75a724c8ca41ec29eba50fb433ffab63db6
zhongmin.wang <zhongmin.wang@lumist.ca>
2022-01-13 19:05:18 +0800

    fix 对含有中横线的环境变量无法识别

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,11 +6,20 @@ else
     echo 'pass!'
 fi
 
-if env | grep -q '^PA_[^=].\+'; then
-    for VAR_NAME in $(env | grep '^PA_[^=].\+' | sed -r "s/^PA_([^=]*).*/\1/g"); do
-        VAL_CONTENT=$(eval echo \${PA_${VAR_NAME}})
-        echo "Config overridden from Environment variable, var=${VAR_NAME}=${VAL_CONTENT}"
-        sed -i -E "s/(${VAR_NAME})(\ *=\ *).*/\1\2${VAL_CONTENT}/i" /app/conf/app.conf
+if env | grep -q '^PA_.\+=.\+'; then
+    for VAR_NAME in $(env | grep '^PA_.\+=.\+' | sed -r "s/^PA_([^=]*).*/\1/g"); do
+        if echo ${VAR_NAME} | grep -q '.*\-.*'; then
+            echo "\"PA_${VAR_NAME}\" in Environment variable contains '-',this will be ignored."
+            continue
+        fi
+        CONF_ITEM=$(grep -Eio "${VAR_NAME/_/-}|${VAR_NAME}" /app/conf/app.conf)
+        if [[ -z ${CONF_ITEM} ]]; then
+            echo "\"PA_${VAR_NAME}\" in Environment variable not found from config file"
+            continue
+        fi
+        CONF_CONTENT=$(eval echo \${PA_${VAR_NAME}})
+        echo "Config overridden from Environment variable, ${CONF_ITEM}=${CONF_CONTENT}."
+        sed -i -E "s@(${CONF_ITEM})(\ *=\ *).*@\1\2${CONF_CONTENT}@i" /app/conf/app.conf
     done
 fi
 
