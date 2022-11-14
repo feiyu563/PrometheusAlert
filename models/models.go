ccb32bd9cd9b2eb1798829f34fd9ccef4263b94b
lusson <luocheng@coding.net>
2022-11-14 15:15:40 +0800

    webhook 添加 form 上传方式
    Signed-off-by: lusson <luocheng@coding.net>

--- models/models.go
+++ models/models.go
@@ -2,18 +2,20 @@ package models
 
 import (
 	"errors"
-	"github.com/astaxie/beego/orm"
 	"time"
+
+	"github.com/astaxie/beego/orm"
 )
 
 // 分类
 type PrometheusAlertDB struct {
-	Id      int
-	Tpltype string //发送类型如钉钉、企业微信、飞书等
-	Tpluse  string //接受目标如Prometheus、WebHook、graylog
-	Tplname string `orm:"index"`
-	Tpl     string `orm:"type(text)"`
-	Created time.Time
+	Id                 int
+	Tpltype            string //发送类型如钉钉、企业微信、飞书等
+	Tpluse             string //接受目标如Prometheus、WebHook、graylog
+	Tplname            string `orm:"index"`
+	Tpl                string `orm:"type(text)"`
+	WebhookContentType string // webhook 请求的 contentType 如 application/json, application/x-www-form-urlencoded 等
+	Created            time.Time
 }
 
 func GetAllTpl() ([]*PrometheusAlertDB, error) {
@@ -64,7 +66,7 @@ func DelTpl(id int) error {
 	return err
 }
 
-func AddTpl(id int, tplname, t_type, t_use, tpl string) error {
+func AddTpl(id int, tplname, t_type, t_use, tpl string, contentType string) error {
 	o := orm.NewOrm()
 	qs := o.QueryTable("PrometheusAlertDB")
 	bExist := qs.Filter("Tplname", tplname).Exist()
@@ -74,19 +76,20 @@ func AddTpl(id int, tplname, t_type, t_use, tpl string) error {
 		return err
 	}
 	Template_table := &PrometheusAlertDB{
-		Id:      id,
-		Tplname: tplname,
-		Tpltype: t_type,
-		Tpluse:  t_use,
-		Tpl:     tpl,
-		Created: time.Now(),
+		Id:                 id,
+		Tplname:            tplname,
+		Tpltype:            t_type,
+		Tpluse:             t_use,
+		Tpl:                tpl,
+		WebhookContentType: contentType,
+		Created:            time.Now(),
 	}
 	// 插入数据
 	_, err = o.Insert(Template_table)
 	return err
 }
 
-func UpdateTpl(id int, tplname, t_type, t_use, tpl string) error {
+func UpdateTpl(id int, tplname, t_type, t_use, tpl string, contentType string) error {
 	o := orm.NewOrm()
 	tpl_update := &PrometheusAlertDB{Id: id}
 	err := o.Read(tpl_update)
@@ -96,6 +99,7 @@ func UpdateTpl(id int, tplname, t_type, t_use, tpl string) error {
 		tpl_update.Tpltype = t_type
 		tpl_update.Tpluse = t_use
 		tpl_update.Tpl = tpl
+		tpl_update.WebhookContentType = contentType
 		tpl_update.Created = time.Now()
 		_, err := o.Update(tpl_update)
 		return err
