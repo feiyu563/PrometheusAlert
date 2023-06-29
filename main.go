c531332298bd4c9bc5e7370accd1eebc139c6cde
cyancow <unix.king@hotmail.com>
2023-06-29 17:46:38 +0800

    main 添加错误处理.

--- main.go
+++ main.go
@@ -32,56 +32,101 @@ func IsExist(path string) bool {
 }
 
 func init() {
-	db_driver := beego.AppConfig.String("db_driver")
-	switch db_driver {
+	dbDriver := beego.AppConfig.String("db_driver")
+	switch dbDriver {
 	case "sqlite3":
 		// 检查数据库文件
-		Db_name := "./db/PrometheusAlertDB.db"
-		if !IsExist(Db_name) {
-			os.MkdirAll(path.Dir(Db_name), os.ModePerm)
-			os.Create(Db_name)
+		DbName := "./db/PrometheusAlertDB.db"
+		if !IsExist(DbName) {
+			err := os.MkdirAll(path.Dir(DbName), os.ModePerm)
+			if err != nil {
+				return
+			}
+			_, err = os.Create(DbName)
+			if err != nil {
+				return
+			}
 		}
 		// 注册驱动（“sqlite3” 属于默认注册，此处代码可省略）
-		orm.RegisterDriver("db_driver", orm.DRSqlite)
+		err := orm.RegisterDriver("db_driver", orm.DRSqlite)
+		if err != nil {
+			return
+		}
 		// 注册默认数据库
-		orm.RegisterDataBase("default", "sqlite3", Db_name, 10)
+		err = orm.RegisterDataBase("default", "sqlite3", DbName, 10)
+		if err != nil {
+			return
+		}
 	case "mysql":
-		orm.RegisterDriver("mysql", orm.DRMySQL)
+		err := orm.RegisterDriver("mysql", orm.DRMySQL)
+		if err != nil {
+			return
+		}
 		dataSource := beego.AppConfig.String("db_user") + ":" + beego.AppConfig.String("db_password") + "@tcp(" + beego.AppConfig.String("db_host") + ":" + beego.AppConfig.String("db_port") + ")/" + beego.AppConfig.String("db_name") + "?charset=utf8mb4"
-		orm.RegisterDataBase("default", "mysql", dataSource)
+		err = orm.RegisterDataBase("default", "mysql", dataSource)
+		if err != nil {
+			return
+		}
 		orm.SetMaxIdleConns("default", 10)
 		orm.SetMaxOpenConns("default", 10)
 	case "postgres":
-		orm.RegisterDriver("postgres", orm.DRPostgres)
+		err := orm.RegisterDriver("postgres", orm.DRPostgres)
+		if err != nil {
+			return
+		}
 		dataSource := "user=" + beego.AppConfig.String("db_user") + " password=" + beego.AppConfig.String("db_password") + " dbname=" + beego.AppConfig.String("db_name") + " host=" + beego.AppConfig.String("db_host") + " port=" + beego.AppConfig.String("db_port") + " sslmode=disable"
-		orm.RegisterDataBase("default", "postgres", dataSource)
+		err = orm.RegisterDataBase("default", "postgres", dataSource)
+		if err != nil {
+			return
+		}
 		orm.SetMaxIdleConns("default", 10)
 		orm.SetMaxOpenConns("default", 10)
 	default:
 		// 检查数据库文件
 		Db_name := "./db/PrometheusAlertDB.db"
 		if !IsExist(Db_name) {
-			os.MkdirAll(path.Dir(Db_name), os.ModePerm)
-			os.Create(Db_name)
+			err := os.MkdirAll(path.Dir(Db_name), os.ModePerm)
+			if err != nil {
+				return
+			}
+			_, err = os.Create(Db_name)
+			if err != nil {
+				return
+			}
 		}
 		// 注册驱动（“sqlite3” 属于默认注册，此处代码可省略）
-		orm.RegisterDriver("db_driver", orm.DRSqlite)
+		err := orm.RegisterDriver("db_driver", orm.DRSqlite)
+		if err != nil {
+			return
+		}
 		// 注册默认数据库
-		orm.RegisterDataBase("default", "sqlite3", Db_name, 10)
+		err = orm.RegisterDataBase("default", "sqlite3", Db_name, 10)
+		if err != nil {
+			return
+		}
 	}
 	// 注册模型
 	orm.RegisterModel(new(models.PrometheusAlertDB), new(models.AlertRecord), new(models.AlertRouter))
-	orm.RunSyncdb("default", false, true)
+	err := orm.RunSyncdb("default", false, true)
+	if err != nil {
+		return
+	}
 }
 
 func main() {
 	orm.Debug = true
 	logtype := beego.AppConfig.String("logtype")
 	if logtype == "console" {
-		logs.SetLogger(logtype)
+		err := logs.SetLogger(logtype)
+		if err != nil {
+			return
+		}
 	} else if logtype == "file" {
 		logpath := beego.AppConfig.String("logpath")
-		logs.SetLogger(logtype, `{"filename":"`+logpath+`"}`)
+		err := logs.SetLogger(logtype, `{"filename":"`+logpath+`"}`)
+		if err != nil {
+			return
+		}
 	}
 	// 输出应用信息
 	logs.Info("[main] 构建的Go版本: %s", GoVersion)
@@ -96,7 +141,10 @@ func main() {
 		RecordLiveDay, _ := beego.AppConfig.Int("RecordLiveDay")
 		logs.Info("[main] 告警记录生存周期: %dd", RecordLiveDay)
 		c := cron.New(cron.WithSeconds())
-		c.AddFunc("0 0 1 * * *", func() { models.RecordCleanByTime(RecordLiveDay) })
+		_, err := c.AddFunc("0 0 1 * * *", func() { models.RecordCleanByTime(RecordLiveDay) })
+		if err != nil {
+			return
+		}
 		c.Start()
 	}
 	models.MetricsInit()
