04bbf5dd4767a9435db96b3c70f4016f5e14231a
jikun.zhang <jikun.zhang@megatronix.co>
2023-07-13 14:38:15 +0800

    fix log error
    Signed-off-by: jikun.zhang <jikun.zhang@megatronix.co>

--- main.go
+++ main.go
@@ -40,31 +40,37 @@ func init() {
 		if !IsExist(DbName) {
 			err := os.MkdirAll(path.Dir(DbName), os.ModePerm)
 			if err != nil {
+				logs.Error(err)
 				return
 			}
 			_, err = os.Create(DbName)
 			if err != nil {
+				logs.Error(err)
 				return
 			}
 		}
 		// 注册驱动（“sqlite3” 属于默认注册，此处代码可省略）
 		err := orm.RegisterDriver("db_driver", orm.DRSqlite)
 		if err != nil {
+			logs.Error(err)
 			return
 		}
 		// 注册默认数据库
 		err = orm.RegisterDataBase("default", "sqlite3", DbName, 10)
 		if err != nil {
+			logs.Error(err)
 			return
 		}
 	case "mysql":
 		err := orm.RegisterDriver("mysql", orm.DRMySQL)
 		if err != nil {
+			logs.Error(err)
 			return
 		}
 		dataSource := beego.AppConfig.String("db_user") + ":" + beego.AppConfig.String("db_password") + "@tcp(" + beego.AppConfig.String("db_host") + ":" + beego.AppConfig.String("db_port") + ")/" + beego.AppConfig.String("db_name") + "?charset=utf8mb4"
 		err = orm.RegisterDataBase("default", "mysql", dataSource)
 		if err != nil {
+			logs.Error(err)
 			return
 		}
 		orm.SetMaxIdleConns("default", 10)
@@ -72,11 +78,13 @@ func init() {
 	case "postgres":
 		err := orm.RegisterDriver("postgres", orm.DRPostgres)
 		if err != nil {
+			logs.Error(err)
 			return
 		}
 		dataSource := "user=" + beego.AppConfig.String("db_user") + " password=" + beego.AppConfig.String("db_password") + " dbname=" + beego.AppConfig.String("db_name") + " host=" + beego.AppConfig.String("db_host") + " port=" + beego.AppConfig.String("db_port") + " sslmode=disable"
 		err = orm.RegisterDataBase("default", "postgres", dataSource)
 		if err != nil {
+			logs.Error(err)
 			return
 		}
 		orm.SetMaxIdleConns("default", 10)
@@ -87,21 +95,25 @@ func init() {
 		if !IsExist(Db_name) {
 			err := os.MkdirAll(path.Dir(Db_name), os.ModePerm)
 			if err != nil {
+				logs.Error(err)
 				return
 			}
 			_, err = os.Create(Db_name)
 			if err != nil {
+				logs.Error(err)
 				return
 			}
 		}
 		// 注册驱动（“sqlite3” 属于默认注册，此处代码可省略）
 		err := orm.RegisterDriver("db_driver", orm.DRSqlite)
 		if err != nil {
+			logs.Error(err)
 			return
 		}
 		// 注册默认数据库
 		err = orm.RegisterDataBase("default", "sqlite3", Db_name, 10)
 		if err != nil {
+			logs.Error(err)
 			return
 		}
 	}
@@ -109,6 +121,7 @@ func init() {
 	orm.RegisterModel(new(models.PrometheusAlertDB), new(models.AlertRecord), new(models.AlertRouter))
 	err := orm.RunSyncdb("default", false, true)
 	if err != nil {
+		logs.Error(err)
 		return
 	}
 }
@@ -117,16 +130,10 @@ func main() {
 	orm.Debug = true
 	logtype := beego.AppConfig.String("logtype")
 	if logtype == "console" {
-		err := logs.SetLogger(logtype)
-		if err != nil {
-			return
-		}
+		logs.SetLogger(logtype)
 	} else if logtype == "file" {
 		logpath := beego.AppConfig.String("logpath")
-		err := logs.SetLogger(logtype, `{"filename":"`+logpath+`"}`)
-		if err != nil {
-			return
-		}
+		logs.SetLogger(logtype, `{"filename":"`+logpath+`"}`)
 	}
 	// 输出应用信息
 	logs.Info("[main] 构建的Go版本: %s", GoVersion)
@@ -143,6 +150,7 @@ func main() {
 		c := cron.New(cron.WithSeconds())
 		_, err := c.AddFunc("0 0 1 * * *", func() { models.RecordCleanByTime(RecordLiveDay) })
 		if err != nil {
+			logs.Error(err)
 			return
 		}
 		c.Start()
