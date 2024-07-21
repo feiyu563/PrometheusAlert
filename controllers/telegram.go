af731fb3359c154cd52f17487447b62a55220b26
聪明大男孩 <boy530@gmail.com>
2024-07-21 20:50:02 +0800

    Update telegram.go
    Markdown对格式要求很严格，修复在用到telegram时选择是否用到markdown

--- controllers/telegram.go
+++ controllers/telegram.go
@@ -21,18 +21,23 @@ func SendTG(msg, logsign string) string {
 	tguserid, _ := beego.AppConfig.Int64("TG_USERID")
 	tgchanname := beego.AppConfig.String("TG_CHANNAME")
 	tgapi := beego.AppConfig.String("TG_API_PROXY")
+	tgParseMode := beego.AppConfig.String("TG_PARSE_MODE")
 
 	botapi := newBot(tgbottoken, logsign, tgapi)
 	var err error
 	if tgmsgmode == "0" {
 		// 推送给个人
 		tgusermsg := tgbotapi.NewMessage(tguserid, msg)
-		tgusermsg.ParseMode = "Markdown" // 设置解析模式为Markdown
+                if tgParseMode == "1" {
+                        tgusermsg.ParseMode = "Markdown" // 设置解析模式为Markdown
+                }
 		_, err = botapi.Send(tgusermsg)
 	} else {
 		// 推送给channel
 		tgchanmsg := tgbotapi.NewMessageToChannel(tgchanname, msg)
-		tgchanmsg.ParseMode = "Markdown" // 设置解析模式为Markdown
+                if tgParseMode == "1" {
+                        tgchanmsg.ParseMode = "Markdown" // 设置解析模式为Markdown
+                }
 		_, err = botapi.Send(tgchanmsg)
 	}
 	if err != nil {
